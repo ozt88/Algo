af9ae3c1e86ae930c07fffef48b602ac2de7ab4e
ozt88 <kl1467@gmail.com>
2018-02-10 16:34:24 +0900

    Reverse Quad

--- Prac/main.cpp
+++ Prac/main.cpp
@@ -20,100 +20,77 @@ using i64 = long long int;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
 
-vector<int> fixedSwitchKey = { 11, 8, 13, 10, 6, 7, 5, 1, 0, 3 };
 
-vector<vector<int>> fixedSwitches =
+struct QuadNode
 {
-	{ 3, 7, 9, 11 },  // only 11
-	{ 6, 7, 8, 10, 12 }, //only 8, 12
-	{ 3, 4, 5, 9, 13 }, // only 13
-	{4, 10, 14, 15}, //only 10
-	{ 0, 4, 5, 6, 7 }, //only 6
-	{ 4, 5, 7, 14, 15 }, //only  7
-	{ 1, 2, 3, 4, 5 }, //only 5
-	{0, 1, 2}, //only 1
-	{ 0, 2, 14, 15 }, // only 0
-	{ 3, 14, 15 }, // only 3
-};
+	char content;
+	vector<QuadNode*> children;
 
-int SolveLikeSudoku(vector<int>& input)
-{
-	vector<int> clocks;
-	for (auto in : input)
-		clocks.push_back((in / 3) % 4);
-
-	if (input[8] != input[12])
-		return -1;
-	
-	int ret = 0;
-	for (int i = 0; i < fixedSwitchKey.size(); ++i)
+	void Reverse()
 	{
-		int count = (4 - clocks[fixedSwitchKey[i]]) % 4;
-		ret += count;
-		for (int idx : fixedSwitches[i])
+		if (children.empty())
+			return;
+
+		iter_swap(children.begin(), children.begin() + 2);
+		iter_swap(children.begin() + 1, children.begin() + 3);
+
+		for (auto child : children)
 		{
-			clocks[idx] = (clocks[idx] + count) % 4;
+			child->Reverse();
 		}
 	}
-
-	for (int c : clocks)
+	
+	string ToString()
 	{
-		if (c % 4)
-			return -1;
+		string ret;
+		ret.push_back(content);
+		for (auto q : children)
+		{
+			ret += q->ToString();
+		}
+		return ret;
 	}
+};
 
-	return ret;
-}
-
-
-void TurnSwitch(vector<int>& clock, int index, int count)
+QuadNode* MakeQuad(string& input)
 {
-	for (auto idx : fixedSwitches[index])
-		clock[idx] = (clock[idx] + count) % 4;
-}
+	if (input.empty())
+		return nullptr;
 
-//10 switch & max 4 case per switch = 4^10 = 1048576 case
-int SolveBrutalIter(vector<int>& clock, int switchIndex)
-{
-	if (switchIndex >= 10)
-		return all_of(clock.begin(), clock.end(), [](int x) { return x == 0; }) ? 0 : -1;
+	auto q = new QuadNode();
+	q->content = input[0];
+	input = input.substr(1, input.length());
 
-	int ret = 0;
-	for (int count = 0; count < 4; ++count)
+	if (q->content == 'x')
 	{
-		TurnSwitch(clock, switchIndex, count);
-		ret = SolveBrutalIter(clock, switchIndex + 1);
-		if (ret >= 0)
-			return ret + count;
+		for (int i = 0; i < 4; ++i)
+		{
+			auto child = MakeQuad(input);
+			if (child == nullptr)
+				break;
 
-		TurnSwitch(clock, switchIndex, 4 - count);
+			q->children.push_back(child);
+		}
 	}
-	return -1;
+	return q;
 }
 
-
-int SolveBrutal(vector<int>& input)
+string Solve(string input)
 {
-	vector<int> clocks;
-	for (auto in : input)
-		clocks.push_back((in / 3) % 4);
-
-	return SolveBrutalIter(clocks, 0);
+	auto q = MakeQuad(input);
+	q->Reverse();
+	return q->ToString();
 }
 
 int main()
 {
 	int C;
 	cin >> C;
-
+	string input;
 	for (int c = 0; c < C; ++c)
 	{
-		vector<int> input(16, 0);
-		for (auto& i : input)
-		{
-			cin >> i;
-		}
-		cout << SolveBrutal(input) << endl;
+		cin >> input;
+		cout << Solve(input) << endl;
 	}
 
 	return 0;
