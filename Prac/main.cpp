cde05df81e697eef6b8b414c8c9b3f156154ce6c
ozt88 <kl1467@gmail.com>
2018-03-10 14:49:34 +0900

    1로 만들기

--- Prac/main.cpp
+++ Prac/main.cpp
@@ -20,95 +20,35 @@ using i64 = long long int;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
 
+const int MAX_NUM = 10000001;
+int cache[MAX_NUM] = { 0, };
 
-bool Check(int x, int y, const vector<pair<int, int>>& candidate, const vector<vector<char>>& board)
+int GetMinCalcCount(int num)
 {
-	for (auto& checkDir : candidate)
-	{
-		int checkX = x + checkDir.first;
-		int checkY = y + checkDir.second;
-
-		if(checkX >= board.size() || checkY >= board[checkX].size() || board[checkX][checkY] != '.')
-			return false;
-	}
-
-	return true;
-}
-
-void Mark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '#';
-}
-
-void Unmark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '.';
-}
-
-bool CheckWholeBoardComplete(const vector<vector<char>>& board, const int H, const int W)
-{
-	for(int h = 0; h < H; ++h)
-	{
-		for (int w = 0; w < W; ++w)
-		{
-			if(board[w][h] == '.')
-				return false;
-		}
-	}
-	return true;
-}
-
-int Iter(int idx, vector<vector<char>>& board, const int H, const int W)
-{
-	if(CheckWholeBoardComplete(board, H, W))
-		return 1;
-
-	if (idx >= H * W)
+	if (num == 1)
 		return 0;
 
-	static const vector<vector<pair<int, int>>> candidates = { {{0, 0}, {0, 1}, {1, 0}}, {{0, 0}, {1, 0}, {1, 1}},  {{0, 0}, {0, 1}, {1, 1}}, {{0, 1}, {1, 0}, {1, 1}}, {}};
-	int x = idx % W, y = idx / W;
-	int result = 0;
-	for (auto& candidate : candidates)
+	int ret = cache[num];
+	if (ret == 0)
 	{
-		if (Check(x, y, candidate, board))
-		{
-			Mark(x, y, candidate, board);
-			if(board[x][y] == '#')
-				result += Iter(idx + 1, board, H, W);
-			Unmark(x, y, candidate, board);
-		}
+		int minValue = GetMinCalcCount(num - 1);
+
+		if (num % 3 == 0)
+			minValue = min(minValue, GetMinCalcCount(num / 3));
+
+		if (num % 2 == 0)
+			minValue = min(minValue, GetMinCalcCount(num / 2));
+
+		ret = minValue + 1;
+		cache[num] = ret;
 	}
-
-	return result;
-}
-
-int Solve(vector<vector<char>>& board, const int H, const int W)
-{
-	return Iter(0, board, H, W);
+	return ret;
 }
 
 int main()
 {
-	int C;
-	cin >> C;
-	for (int i = 0; i < C; ++i)
-	{
-		int H, W;
-		cin >> H >> W;
-		vector<vector<char>> board(W, vector<char>(H));
-		for (int h = 0; h < H; ++h)
-		{
-			for (int w = 0; w < W; ++w)
-			{
-				cin >> board[w][h];
-			}
-		}
-		int result = Solve(board, H, W);
-		cout <<  result << endl;
-	}
-
+	int X;
+	cin >> X;
+	cout << GetMinCalcCount(X);
 	return 0;
 }
