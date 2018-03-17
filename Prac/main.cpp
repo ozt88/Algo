31c1e6e6072ae1e97e6e56cc8397e3c89a55aa85
ozt88 <kl1467@gmail.com>
2018-03-17 16:58:38 +0900

    Update main.cpp

--- Prac/main.cpp
+++ Prac/main.cpp
@@ -20,94 +20,50 @@ using i64 = long long int;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
 
+int N;
+int list[500];
+int cache[500];
 
-bool Check(int x, int y, const vector<pair<int, int>>& candidate, const vector<vector<char>>& board)
+int GetLongestSequence(int index)
 {
-	for (auto& checkDir : candidate)
-	{
-		int checkX = x + checkDir.first;
-		int checkY = y + checkDir.second;
-
-		if(checkX >= board.size() || checkY >= board[checkX].size() || board[checkX][checkY] != '.')
-			return false;
-	}
-
-	return true;
-}
-
-void Mark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '#';
-}
-
-void Unmark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '.';
-}
-
-bool CheckWholeBoardComplete(const vector<vector<char>>& board, const int H, const int W)
-{
-	for(int h = 0; h < H; ++h)
-	{
-		for (int w = 0; w < W; ++w)
-		{
-			if(board[w][h] == '.')
-				return false;
-		}
-	}
-	return true;
-}
-
-int Iter(int idx, vector<vector<char>>& board, const int H, const int W)
-{
-	if(CheckWholeBoardComplete(board, H, W))
-		return 1;
-
-	if (idx >= H * W)
+	if (index == N)
 		return 0;
 
-	static const vector<vector<pair<int, int>>> candidates = { {{0, 0}, {0, 1}, {1, 0}}, {{0, 0}, {1, 0}, {1, 1}},  {{0, 0}, {0, 1}, {1, 1}}, {{0, 1}, {1, 0}, {1, 1}}, {}};
-	int x = idx % W, y = idx / W;
-	int result = 0;
-	for (auto& candidate : candidates)
+	if (cache[index] != -1)
+		return cache[index];
+
+	int& ret = cache[index];
+	ret = 1;
+	for (int i = index + 1; i < N; ++i)
 	{
-		if (Check(x, y, candidate, board))
-		{
-			Mark(x, y, candidate, board);
-			if(board[x][y] == '#')
-				result += Iter(idx + 1, board, H, W);
-			Unmark(x, y, candidate, board);
-		}
+		if(list[index] < list[i])
+			ret = max(ret, GetLongestSequence(i) + 1);
 	}
-
-	return result;
+	return ret;
 }
 
-int Solve(vector<vector<char>>& board, const int H, const int W)
-{
-	return Iter(0, board, H, W);
-}
 
 int main()
 {
 	int C;
 	cin >> C;
-	for (int i = 0; i < C; ++i)
+	for (int c = 0; c < C; ++c)
 	{
-		int H, W;
-		cin >> H >> W;
-		vector<vector<char>> board(W, vector<char>(H));
-		for (int h = 0; h < H; ++h)
+		memset(list, 0, sizeof(list));
+		memset(cache, -1, sizeof(cache));
+
+		cin >> N;
+		for (int i = 0; i < N; ++i)
 		{
-			for (int w = 0; w < W; ++w)
-			{
-				cin >> board[w][h];
-			}
+			cin >> list[i];
 		}
-		int result = Solve(board, H, W);
-		cout <<  result << endl;
+
+		int ret = 0;
+		for (int i = 0; i < N; ++i)
+		{
+			ret = max(ret, GetLongestSequence(i));
+		}
+		cout << ret << endl;
 	}
 
 	return 0;
