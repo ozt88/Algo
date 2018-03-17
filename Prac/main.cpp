6c81ab89297c26221c0e600ddf8a648e472e60d0
ozt88 <kl1467@gmail.com>
2018-03-17 15:17:07 +0900

    Update main.cpp

--- Prac/main.cpp
+++ Prac/main.cpp
@@ -20,94 +20,42 @@ using i64 = long long int;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
 
+int N = 0;
+int triangle[100][100] = { 0, };
+int cache[100][100] = { 0, };
 
-bool Check(int x, int y, const vector<pair<int, int>>& candidate, const vector<vector<char>>& board)
+
+int GetBiggestPath(int x, int y)
 {
-	for (auto& checkDir : candidate)
-	{
-		int checkX = x + checkDir.first;
-		int checkY = y + checkDir.second;
-
-		if(checkX >= board.size() || checkY >= board[checkX].size() || board[checkX][checkY] != '.')
-			return false;
-	}
-
-	return true;
+	if (y == N - 1)
+		return triangle[x][y];
+	
+	if (cache[x][y] != 0)
+		return cache[x][y];
+	
+	cache[x][y] = max(GetBiggestPath(x, y + 1), GetBiggestPath(x + 1, y + 1)) + triangle[x][y];	
+	return cache[x][y];
 }
 
-void Mark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '#';
-}
-
-void Unmark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '.';
-}
-
-bool CheckWholeBoardComplete(const vector<vector<char>>& board, const int H, const int W)
-{
-	for(int h = 0; h < H; ++h)
-	{
-		for (int w = 0; w < W; ++w)
-		{
-			if(board[w][h] == '.')
-				return false;
-		}
-	}
-	return true;
-}
-
-int Iter(int idx, vector<vector<char>>& board, const int H, const int W)
-{
-	if(CheckWholeBoardComplete(board, H, W))
-		return 1;
-
-	if (idx >= H * W)
-		return 0;
-
-	static const vector<vector<pair<int, int>>> candidates = { {{0, 0}, {0, 1}, {1, 0}}, {{0, 0}, {1, 0}, {1, 1}},  {{0, 0}, {0, 1}, {1, 1}}, {{0, 1}, {1, 0}, {1, 1}}, {}};
-	int x = idx % W, y = idx / W;
-	int result = 0;
-	for (auto& candidate : candidates)
-	{
-		if (Check(x, y, candidate, board))
-		{
-			Mark(x, y, candidate, board);
-			if(board[x][y] == '#')
-				result += Iter(idx + 1, board, H, W);
-			Unmark(x, y, candidate, board);
-		}
-	}
-
-	return result;
-}
-
-int Solve(vector<vector<char>>& board, const int H, const int W)
-{
-	return Iter(0, board, H, W);
-}
 
 int main()
 {
 	int C;
 	cin >> C;
-	for (int i = 0; i < C; ++i)
+	for (int c = 0; c < C; ++c)
 	{
-		int H, W;
-		cin >> H >> W;
-		vector<vector<char>> board(W, vector<char>(H));
-		for (int h = 0; h < H; ++h)
+		memset(triangle, 0, sizeof(triangle));
+		memset(cache, 0, sizeof(triangle));
+
+		cin >> N;
+		for (int y = 0; y < N; ++y)
 		{
-			for (int w = 0; w < W; ++w)
+			for (int x = 0; x <= y; ++x)
 			{
-				cin >> board[w][h];
+				cin >> triangle[x][y];
 			}
 		}
-		int result = Solve(board, H, W);
-		cout <<  result << endl;
+		cout << GetBiggestPath(0, 0) << endl;
 	}
 
 	return 0;
