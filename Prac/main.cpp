6118cfec5bf1ed430b799374061a261b15795bb1
ozt88 <kl1467@gmail.com>
2018-04-07 15:51:32 +0900

    Update main.cpp

--- Prac/main.cpp
+++ Prac/main.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <vector>
 #include <queue>
@@ -20,95 +21,55 @@ using i64 = long long int;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
 
-
-bool Check(int x, int y, const vector<pair<int, int>>& candidate, const vector<vector<char>>& board)
+int getDamage(const string& P)
 {
-	for (auto& checkDir : candidate)
+	int damageSum = 0;
+	int curDamage = 1;
+	for (auto c : P)
 	{
-		int checkX = x + checkDir.first;
-		int checkY = y + checkDir.second;
-
-		if(checkX >= board.size() || checkY >= board[checkX].size() || board[checkX][checkY] != '.')
-			return false;
+		if (c == 'C')
+			curDamage *= 2;
+		else if (c == 'S')
+			damageSum += curDamage;
 	}
-
-	return true;
+	return damageSum;
 }
 
-void Mark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
+string solve(int D, string& P)
 {
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '#';
-}
+	if (getDamage(P) <= D)
+		return to_string(0);
 
-void Unmark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '.';
-}
-
-bool CheckWholeBoardComplete(const vector<vector<char>>& board, const int H, const int W)
-{
-	for(int h = 0; h < H; ++h)
+	int moveCount = 0;
+	size_t lastSPos, lastMovableCPos;
+	while (true) 
 	{
-		for (int w = 0; w < W; ++w)
+		lastSPos = P.find_last_of('S');
+		lastMovableCPos = P.find_last_of('C', lastSPos);
+		if (lastMovableCPos == string::npos)
+			return "IMPOSSIBLE";
+
+		for (;lastMovableCPos < lastSPos; ++lastMovableCPos)
 		{
-			if(board[w][h] == '.')
-				return false;
+			moveCount++;
+			iter_swap(P.begin() + lastMovableCPos, P.begin() + lastMovableCPos + 1 );
+
+			if (getDamage(P) <= D)
+				return to_string(moveCount);
 		}
 	}
-	return true;
-}
-
-int Iter(int idx, vector<vector<char>>& board, const int H, const int W)
-{
-	if(CheckWholeBoardComplete(board, H, W))
-		return 1;
-
-	if (idx >= H * W)
-		return 0;
-
-	static const vector<vector<pair<int, int>>> candidates = { {{0, 0}, {0, 1}, {1, 0}}, {{0, 0}, {1, 0}, {1, 1}},  {{0, 0}, {0, 1}, {1, 1}}, {{0, 1}, {1, 0}, {1, 1}}, {}};
-	int x = idx % W, y = idx / W;
-	int result = 0;
-	for (auto& candidate : candidates)
-	{
-		if (Check(x, y, candidate, board))
-		{
-			Mark(x, y, candidate, board);
-			if(board[x][y] == '#')
-				result += Iter(idx + 1, board, H, W);
-			Unmark(x, y, candidate, board);
-		}
-	}
-
-	return result;
-}
-
-int Solve(vector<vector<char>>& board, const int H, const int W)
-{
-	return Iter(0, board, H, W);
 }
 
 int main()
-{
-	int C;
-	cin >> C;
-	for (int i = 0; i < C; ++i)
+{	
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; ++i)
 	{
-		int H, W;
-		cin >> H >> W;
-		vector<vector<char>> board(W, vector<char>(H));
-		for (int h = 0; h < H; ++h)
-		{
-			for (int w = 0; w < W; ++w)
-			{
-				cin >> board[w][h];
-			}
-		}
-		int result = Solve(board, H, W);
-		cout <<  result << endl;
+		int D;
+		string P;
+		cin >> D >> P;
+		cout << "Case #" << i + 1 << ": " << solve(D, P) << endl;
 	}
-
 	return 0;
 }
