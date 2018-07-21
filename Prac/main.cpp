f021a6954c9bfcd5498fb8870231d7a672503681
ozt88 <kl1467@gmail.com>
2018-07-21 13:48:40 +0900

    parametric search

--- Prac/main.cpp
+++ Prac/main.cpp
@@ -13,6 +13,7 @@
 #include <random>
 #include <functional>
 #include <assert.h>
+#include <math.h>
 
 using namespace std;
 
@@ -20,95 +21,50 @@ using i64 = long long int;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
 
-
-bool Check(int x, int y, const vector<pair<int, int>>& candidate, const vector<vector<char>>& board)
+i64 search(i64 min, i64 max, function<bool(i64)> check)
 {
-	for (auto& checkDir : candidate)
+	i64 low = min;
+	i64 high = max;
+	i64 ret = 0;
+	while (low <= high)
 	{
-		int checkX = x + checkDir.first;
-		int checkY = y + checkDir.second;
-
-		if(checkX >= board.size() || checkY >= board[checkX].size() || board[checkX][checkY] != '.')
-			return false;
-	}
-
-	return true;
-}
-
-void Mark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '#';
-}
-
-void Unmark(int x, int y, const vector<pair<int, int>>& candidate, vector<vector<char>>& board)
-{
-	for (auto& checkDir : candidate)
-		board[x + checkDir.first][y + checkDir.second] = '.';
-}
-
-bool CheckWholeBoardComplete(const vector<vector<char>>& board, const int H, const int W)
-{
-	for(int h = 0; h < H; ++h)
-	{
-		for (int w = 0; w < W; ++w)
+		i64 mid = (low + high) / 2;
+		if (check(mid))
 		{
-			if(board[w][h] == '.')
-				return false;
+			low = mid + 1;
+			ret = mid;
+		}
+		else
+		{
+			high = mid - 1;
 		}
 	}
-	return true;
-}
-
-int Iter(int idx, vector<vector<char>>& board, const int H, const int W)
-{
-	if(CheckWholeBoardComplete(board, H, W))
-		return 1;
-
-	if (idx >= H * W)
-		return 0;
-
-	static const vector<vector<pair<int, int>>> candidates = { {{0, 0}, {0, 1}, {1, 0}}, {{0, 0}, {1, 0}, {1, 1}},  {{0, 0}, {0, 1}, {1, 1}}, {{0, 1}, {1, 0}, {1, 1}}, {}};
-	int x = idx % W, y = idx / W;
-	int result = 0;
-	for (auto& candidate : candidates)
-	{
-		if (Check(x, y, candidate, board))
-		{
-			Mark(x, y, candidate, board);
-			if(board[x][y] == '#')
-				result += Iter(idx + 1, board, H, W);
-			Unmark(x, y, candidate, board);
-		}
-	}
-
-	return result;
-}
-
-int Solve(vector<vector<char>>& board, const int H, const int W)
-{
-	return Iter(0, board, H, W);
+	return ret;
 }
 
 int main()
 {
-	int C;
-	cin >> C;
-	for (int i = 0; i < C; ++i)
+	i64 N, M;
+	cin >> N >> M;
+	vector<i64> heights(N);
+	for (auto& height : heights)
 	{
-		int H, W;
-		cin >> H >> W;
-		vector<vector<char>> board(W, vector<char>(H));
-		for (int h = 0; h < H; ++h)
+		cin >> height;
+	}
+	const i64 MAX = 1000000000;
+	
+	cout << search(0, MAX, [&heights, &M](i64 cut)
+	{
+		i64 sum = 0;
+		for (auto& h : heights)
 		{
-			for (int w = 0; w < W; ++w)
+			i64 sub = h - cut;
+			if (sub > 0)
 			{
-				cin >> board[w][h];
+				sum += sub;
 			}
 		}
-		int result = Solve(board, H, W);
-		cout <<  result << endl;
-	}
-
+		return sum >= M;
+	});
 	return 0;
 }
